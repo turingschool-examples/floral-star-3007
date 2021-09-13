d94b6248c493bf9cf027f4a84df37714834c078f
Ida Olson <idaolson@gmail.com>
2021-09-13 10:05:24 -0600

    Finish tests for user story 2

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'the mechanics show page' do
   before(:each) do
     @park = AmusementPark.create!(name: "State Fair", price_of_admission: 12.99)
     @mech1 = Mechanic.create!(name: "Ida Olson", years_experience: 14)
-    @mech2 = Mechanic.create!(name: "Sarah Carter", years_xperience: 10)
+    @mech2 = Mechanic.create!(name: "Sarah Carter", years_experience: 10)
     @ride1 = Ride.create!(name: "The Mangler", thrill_rating: 6, open: true, amusement_park_id: @park.id)
     @ride2 = Ride.create!(name: "Puke-A-Thon", thrill_rating: 8, open: true, amusement_park_id: @park.id)
     @ride3 = Ride.create!(name: "Topsy Scurvy", thrill_rating: 3, open: false, amusement_park_id: @park.id)
@@ -17,8 +17,26 @@ RSpec.describe 'the mechanics show page' do
     visit mechanic_path(@mech1)
 
     expect(page).to have_content(@mech1.name)
-    expect(page).to have_content(@mech1.years_of_experience)
+    expect(page).to have_content(@mech1.years_experience)
     expect(page).to have_content(@ride1.name)
   end
 
+  it "only shows the mechanics rides that are open" do
+    @mech1.rides << @ride1
+    @mech1.rides << @ride3
+
+    visit mechanic_path(@mech1)
+
+    expect(page).to have_content(@ride1.name)
+    expect(page).to have_no_content(@ride3.name)
+  end
+
+  it "shows the rides by thrill rating it descending order" do
+    @mech1.rides << @ride1
+    @mech1.rides << @ride2
+
+    visit mechanic_path(@mech1)
+
+    expect(@ride2.name).to appear_before(@ride1.name)
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,10 +6,23 @@ RSpec.describe Mechanic do
     it { should have_many(:rides).through(:mechanic_rides) }
   end
 
-  it "averages years of experience" do
+  it "averages years experience" do
     mech1 = Mechanic.create!(name: "Ida Olson", years_experience: 14)
     mech2 = Mechanic.create!(name: "Sarah Carter", years_experience: 10)
 
     expect(Mechanic.average_experience).to eq(12)
+  end
+
+  it "only returns a mechanic's rides that are open and ranks them by thrill rating" do
+    park = AmusementPark.create!(name: "State Fair", price_of_admission: 12.99)
+    mech1 = Mechanic.create!(name: "Ida Olson", years_experience: 14)
+    ride1 = Ride.create!(name: "The Mangler", thrill_rating: 6, open: true, amusement_park_id: park.id)
+    ride2 = Ride.create!(name: "Puke-A-Thon", thrill_rating: 8, open: true, amusement_park_id: park.id)
+    ride3 = Ride.create!(name: "Topsy Scurvy", thrill_rating: 3, open: false, amusement_park_id: park.id)
+    mech1.rides << ride1
+    mech1.rides << ride2
+    mech1.rides << ride3
+
+    expect(mech1.open_rides).to eq([ride2, ride1])
   end
 end
