c0812dd65cef3a3c34ab8c9c1a135ab4b824a08f
Jamie Pace <jamiejpace@gmail.com>
2021-09-13 09:38:06 -0600

    Complete first user story

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Mechanic do
     it { should have_many :ride_mechanics}
     it { should have_many(:rides).through(:ride_mechanics) }
   end
+
+  describe 'class methods' do
+    describe '#ave_experience' do
+      it 'caculates average experience for all mechanics' do
+        mechanic1 = Mechanic.create!(name: "Gypsy", years_of_experience: 15)
+        mechanic2 = Mechanic.create!(name: "Daisy", years_of_experience: 10)
+        mechanic3 = Mechanic.create!(name: "Rosie", years_of_experience: 20)
+
+        expect(Mechanic.ave_experience).to eq(15.0)
+      end
+    end
+  end
 end
