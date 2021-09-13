f6d74ef739f5b7b8a106ca2210c80ff042ecfa4c
Christina Delpone <cdelpone@gmail.com>
2021-09-13 10:10:44 -0600

    Test average years of experience class method and view

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 # rspec spec/features/mechanics/index_spec.rb
 RSpec.describe 'Mechanic Index Page' do
+  before :each do
+    # @fun_park = AmusementPark.create!(name: "Fun Park", price_of_admission: 10)
+    # @best_ride = fun_park.rides.create!(name: "Best Ride", thrill_rating: 2, open: true)
+    @sara = Mechanic.create!(name: "Sara", experience: 20)
+    @fred = Mechanic.create!(name: "Fred", experience: 10)
+
+    visit mechanics_path
+  end
   describe 'display all mechanics and their attributes' do
     it 'lists all mechanics and their attributes' do
-      fun_park = AmusementPark.create!(name: "Fun Park", price_of_admission: 10)
-      best_ride = fun_park.rides.create!(name: "Best Ride", thrill_rating: 2, open: true)
-      sara = Mechanic.create!(name: "Sara", experience: 13)
-      fred = Mechanic.create!(name: "Fred", experience: 13)
-
-      visit mechanics_path
-
       expect(page).to have_content("All Mechanics")
-      expect(page).to have_content(sara.name)
-      expect(page).to have_content(sara.experience)
-      expect(page).to have_content(fred.name)
-      expect(page).to have_content(fred.experience)
+      expect(page).to have_content(@sara.name)
+      expect(page).to have_content(@sara.experience)
+      expect(page).to have_content(@fred.name)
+      expect(page).to have_content(@fred.experience)
     end
   end
 
   describe 'display average years of experience' do
     it 'calcs the average years of experience across all mechanics' do
+
+      expect(page).to have_content("Average years of experience: 15")
     end
   end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  describe 'class method' do
+    it 'calcs the average years of experience across all mechanics' do
+      sara = Mechanic.create!(name: "Sara", experience: 20)
+      fred = Mechanic.create!(name: "Fred", experience: 10)
+
+      expect(Mechanic.avg_experience).to eq(15)
+    end
+  end
 end
