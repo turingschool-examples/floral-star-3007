567165c9c99208f48304ceebd2c75818c2135d8e
Rteske <ryanteske@outlook.com>
2021-09-13 09:01:21 -0700

    Add mechanic index

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Mechanics Index Page' do
+    before :each do
+        mech_1 = Mechanic.create!({name: "Dog", years_experience: 4})
+        mech_2 = Mechanic.create!({name: "Bird", years_experience: 6})
+        mech_3 = Mechanic.create!({name: "Squirel", years_experience: 20})
+
+        visit("/mechanics")
+    end
+    describe 'display' do
+        it 'has a header and all the mechanics and thier attributtes' do
+            expect(page).to have_content("Dog")
+            expect(page).to have_content("4")
+            expect(page).to have_content("Bird")
+            expect(page).to have_content("6")
+            expect(page).to have_content("Squirel")
+            expect(page).to have_content("20")
+        end
+    end
+end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic do
+    before :each do
+        mech_1 = Mechanic.create!({name: "Dog", years_experience: 4})  
+        mech_2 = Mechanic.create!({name: "Bird", years_experience: 6})
+        mech_3 = Mechanic.create!({name: "Squirel", years_experience: 20})      
+    end
+
     describe 'Relationships' do
         it { should have_many(:mechanic_rides)}
         it { should have_many(:rides).through(:mechanic_rides)}
+    end
+
+    describe 'Instance Methods' do
+
+    end
+
+    describe 'Class Methods' do
+        it 'should take the average of all years experience' do
+            expect(Mechanic.avg_years_experience).to eq(10)
+        end
     end
 end
