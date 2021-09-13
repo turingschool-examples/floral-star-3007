6dd227312cbd9419e1398b9660d29e244279cb23
Christina Delpone <cdelpone@gmail.com>
2021-09-13 11:44:36 -0600

    test app show page

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -5,26 +5,29 @@ RSpec.describe 'Mechanic Show Page' do
     @fun_park = AmusementPark.create!(name: "Fun Park", price_of_admission: 10)
     @best_ride = @fun_park.rides.create!(name: "Best Ride", thrill_rating: 2, open: true)
     @scariest_ride = @fun_park.rides.create!(name: "Scariest Ride", thrill_rating: 10, open: true)
-    @closed_ride = @fun_park.rides.create!(name: "Closed Ride", thrill_rating: 6, open: false)
     @sara = Mechanic.create!(name: "Sara", experience: 20)
     @fred = Mechanic.create!(name: "Fred", experience: 10)
 
   end
   describe 'display mechanics attributes and rides their working on' do
     it 'has name, years of experience, name of rides working on' do
-      visit mechanic_path(@sara)
+      visit mechanic_path(@sara.id)
 
-      MechanicRide.create!(ride: @best_ride, mechanic: @sara)
-      MechanicRide.create!(ride: @scariest_ride, mechanic: @sara)
-      MechanicRide.create!(ride: @closed_ride, mechanic: @sara)
+      @sara.rides << @best_ride
+      @sara.rides << @scariest_ride
+      # MechanicRide.create!(ride: @best_ride, mechanic: @sara)
+      # MechanicRide.create!(ride: @scariest_ride, mechanic: @sara)
 
       expect(page).to have_content(@sara.name)
       expect(page).to have_content(@sara.experience)
-      expect(@sara.rides).to eq([@best_ride, @scariest_ride, @closed_ride])
+      expect(page).to have_content(@best_ride.name)
+      expect(page).to have_content("Best Ride")
+      expect(page).to have_content("Scariest Ride")
     end
 
     it 'only shows open rides' do
       visit mechanic_path(@sara)
+      @closed_ride = @fun_park.rides.create!(name: "Closed Ride", thrill_rating: 6, open: false)
 
       MechanicRide.create!(ride: @best_ride, mechanic: @sara)
       MechanicRide.create!(ride: @scariest_ride, mechanic: @sara)
@@ -32,10 +35,12 @@ RSpec.describe 'Mechanic Show Page' do
 
       expect(page).to have_content(@sara.name)
       expect(page).to have_content(@sara.experience)
-      expect(@sara.rides).to eq([@best_ride, @scariest_ride])
+      expect(@sara.rides.available).to eq([@best_ride, @scariest_ride])
     end
 
     it 'lists the rides by desc thrill rating' do
+      expect(@scariest_ride.name).to appear_before(@best_ride.name)
+
       # And the rides are listed by thrill rating in descending order (most thrills first)
     end
   end
