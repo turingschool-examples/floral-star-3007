a9833771c29a39f1a5ab0a70b65e95f9e2ffb8cc
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-13 11:21:23 -0600

    Add amusement park show page route with action in controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/mechanics/:mech_id', to: 'mechanics#show'
   patch '/mechanics/:mech_id', to: 'mechanics#update'
+
+  get '/amusement_parks/:park_id', to: 'amusement_parks#show'
 end
