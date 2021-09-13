0ac9bf971389d3ee9802760417537a480eac53e8
Ozzie <mikeosmonson@gmail.com>
2021-09-13 09:30:44 -0600

    Add Ride relationships, validations, and tests.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,9 @@
 class Ride < ApplicationRecord
   belongs_to :amusement_park
+  has_many :ride_mechanics, dependent: :destroy
+  has_many :mechanics, through: :ride_mechanics
+
+  validates :name, presence: true
+  validates :thrill_rating, presence: true
+  validates :open, inclusion: { in: [true, false] }
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe Ride do
   describe 'relationships' do
     it { should belong_to(:amusement_park) }
+    it { should have_many(:ride_mechanics) }
+    it { should have_many(:mechanics), through: :ride_mechanics }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:thrill_rating) }
+    it { should allow_value(true).for (:open) }
+    it { should allow_value(false).for (:open) }
   end
 end
