0c5e83b1533221cc9e835c8f686e623e13e7479c
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-13 10:24:20 -0600

    Add spec for mechanic show page with test for attributes, open rides only, ordered by thrill

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'mechanics show page' do
+  before(:each) do
+    @cp = AmusementPark.create!(name: "Cedar Point", price_of_admission: 60)
+    @kara = Mechanic.create!(name: "Kara Johnson", yrs_xp: 12)
+    @jamie = Mechanic.create!(name: "Jamie Marsh", yrs_xp: 5)
+    @willie = Mechanic.create!(name: "Willie Walsh", yrs_xp: 32)
+    @topthrill = Ride.create!(name: "Top Thrill Dragster", thrill_rating: 9, open: false, amusement_park: @cp)
+    @maverick = Ride.create!(name: "Maverick", thrill_rating: 8, open: true, amusement_park: @cp)
+    @millenium = Ride.create!(name: "Millenium Force", thrill_rating: 10, open: true, amusement_park: @cp)
+  end
+
+  it 'displays mechanic attributes and open rides' do
+    @kara.rides << @maverick
+    @kara.rides << @millenium
+    @kara.rides << @topthrill
+    visit "/mechanics/#{@kara.id}"
+
+    expect(page).to have_content(@kara.name)
+    expect(page).to have_content(@kara.yrs_xp)
+
+    within("#rides") do
+      expect(page).to have_content(@maverick.name)
+      expect(page).to have_content(@millenium.name)
+      expect(page).to_not have_content(@topthrill.name)
+
+      expect(@millenium.name).to appear_before(@maverick.name, only_text: true)
+    end
+    expect(page).to_not have_content(@jamie.name)
+    expect(page).to_not have_content(@willie.name)
+  end
+end
