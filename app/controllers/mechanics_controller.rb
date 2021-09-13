2c831b8c8d8dc904dbf24422acd9ff52e46e8f1e
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-13 10:24:53 -0600

    Add route, action, and view for mechanic show page

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,4 +2,9 @@ class MechanicsController < ApplicationController
   def index
     @mechanics = Mechanic.all
   end
+
+  def show
+    @mechanic = Mechanic.find(params[:mech_id])
+    @ordered_rides = @mechanic.rides.order_by_thrill
+  end
 end
