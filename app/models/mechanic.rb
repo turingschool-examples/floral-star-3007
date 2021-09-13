e21dd267d519c3f9196c69e8b9173f7e29356c91
Laura Court <laura.mcourt@gmail.com>
2021-09-13 11:49:32 -0400

    Add years experience method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics, dependent: :destroy
   has_many :rides, through: :ride_mechanics
+
+  def self.average_years
+    average(:years_of_experience).round
+  end
 end
