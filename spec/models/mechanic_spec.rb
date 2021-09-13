19da6bf3e1593c0e3e4270d0b689993021227ba3
Weston Ellis <ellisweston112@gmail.com>
2021-09-13 09:42:36 -0600

    Write model test

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Mechanic do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  describe 'class methods' do
+    it 'average experience' do
+      mech1 = Mechanic.create!(name: "Bill", years_experience: 2)
+      mech2 = Mechanic.create!(name: "Janet", years_experience: 7)
+
+      expect(Mechanic.average_experience).to eq(4.5)
+    end
+  end
 end
