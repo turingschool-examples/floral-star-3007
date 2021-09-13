3509c3c0ac90852ae0c4af624bf92891ecab2a10
Ida Olson <idaolson@gmail.com>
2021-09-13 09:18:50 -0600

    Edit mechanic spec

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic do
-  it { should have_many(:mechanic_rides) }
-  it { should have_many(:rides).through(:mechanic_rides) }
+  describe 'relationships' do
+    it { should have_many(:mechanic_rides) }
+    it { should have_many(:rides).through(:mechanic_rides) }
+  end
 end
