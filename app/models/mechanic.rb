4616ca4756e23ecdd77533b9c1abd2c688469ae1
Weston Ellis <ellisweston112@gmail.com>
2021-09-13 09:43:36 -0600

    Write mechanic method

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,4 +2,9 @@ class Mechanic < ApplicationRecord
   has_many  :mechanic_rides
   has_many  :rides, through: :mechanic_rides
 
+  def self.average_experience
+    average(:years_experience)
+  end
+
+
 end
