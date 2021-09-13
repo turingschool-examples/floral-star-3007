d9a130a6e269d9300d64b0a8c7be6b5ec15485d7
Ted S <tstaros23@gmail.com>
2021-09-13 10:31:59 -0600

    Finish user story 1

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :mechanic_rides}
     it { should have_many(:rides).through(:mechanic_rides)}
   end
+
+  describe "methods" do
+    it "#average_years_of_experience" do
+      mechanic_1 = Mechanic.create!(name: 'Ted', years_experience: 9)
+      mechanic_2 = Mechanic.create!(name: 'Bob', years_experience: 11)
+
+      expect(Mechanic.average_years_of_experience).to eq(10)
+    end
+  end
 end
