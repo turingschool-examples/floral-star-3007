6b148a5d2d80c7a3a81af9f823f141a35987bcd2
Tanner Dale <tanner@tannerdale.com>
2021-09-13 10:54:48 -0500

    Add mechanic instance method open_rides, show view

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
   scope :average_experience, -> {
     average(:years_of_experience)
   }
+
+  def open_rides
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
