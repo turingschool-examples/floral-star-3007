e69b41088ed5e04f9397b09d7aab92549c1a7298
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-13 10:20:47 -0600

    Add order_by_thrill class method

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,8 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :mechanic_rides, dependent: :destroy
   has_many :mechanics, through: :mechanic_rides
+
+  def self.order_by_thrill
+    order(thrill_rating: :desc)
+  end
 end
