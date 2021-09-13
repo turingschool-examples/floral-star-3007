68ed980beba1ca9b2c4b3a6c5ceae1c68dab40f0
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-13 09:39:19 -0600

    Add list of mechanics

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'the mechanics index page' do
   context 'when I visit the mechanics index page' do
     before(:each) do
+      @speed     = Mechanic.create!(name: 'Speed Racer', years_experience: 5)
+      @trixie    = Mechanic.create!(name: 'Trixie',      years_experience: 4)
+      @chimchim  = Mechanic.create!(name: 'Chim-Chim',   years_experience: 2)
+
       visit '/mechanics/'
     end
 
@@ -10,10 +14,15 @@ RSpec.describe 'the mechanics index page' do
       expect(page).to have_content('All Mechanics')
     end
 
-    # it 'shows a ist of all mechanics names and their years of experience' do
-    #
-    # end
-    #
+    it 'shows a list of all mechanics names and their years of experience' do
+      expect(page).to have_content('Speed Racer')
+      expect(page).to have_content('Years of Experience: 5')
+      expect(page).to have_content('Trixie')
+      expect(page).to have_content('Years of Experience: 4')
+      expect(page).to have_content('Chim-Chim')
+      expect(page).to have_content('Years of Experience: 2')
+    end
+
     # it 'shows the average years of experience across all mechanics' do
     #
     # end
