1b6e92746aea6a76cc790d7d5adcceafdf8d3ad8
Ida Olson <idaolson@gmail.com>
2021-09-13 10:50:34 -0600

    Update test

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe 'the amusement park show page' do
 
   it "shows the average thrill rating of the rides" do
     visit amusement_park_path(@park1)
-
+    # save_and_open_page
     expect(page).to have_content("Average Thrill Rating: 4")
   end
 end
