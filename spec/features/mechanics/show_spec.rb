6671e35ad535a81b2c0d1dc2d2c2c492a645a263
Weston Ellis <ellisweston112@gmail.com>
2021-09-13 09:54:33 -0600

    Write story 2 test

---
+++ spec/features/mechanics/show_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe 'mechanic show page' do
+
+  it 'story 2' do
+    mech = Mechanic.create!(name: "Janet", years_experience: 7)
+    park = AmusementPark.create!(name: 'Yada Thrills', price_of_admission: 4)
+    ride1 = park.rides.create!(name: 'Hurler', thrill_rating: 10, open: true)
+    ride2 = park.rides.create!(name: 'Lazy', thrill_rating: 1, open: true)
+    ride3 = park.rides.create!(name: 'Tosser', thrill_rating: 5, open: true)
+    ride4 = park.rides.create!(name: 'Null', thrill_rating: 0, open: false)
+    ride5 = park.rides.create!(name: 'Null2', thrill_rating: 0, open: true)
+
+    mech.rides << ride1
+    mech.rides << ride2
+    mech.rides << ride3
+    mech.rides << ride4
+
+    visit "/mechanics/#{mech.id}"
+
+    expect(page).to have_content(mech.name)
+    expect(page).to have_content(mech.years_experience)
+    expect(page).to have_content(ride1.name)
+    expect(page).to have_content(ride2.name)
+    expect(page).to have_content(ride3.name)
+
+    expect(page).to_not have_content(ride4.name)
+    expect(page).to_not have_content(ride5.name)
+
+    expect(ride1.name).to appear_before(ride2.name)
+    expect(ride1.name).to appear_before(ride2.name)
+    expect(ride3.name).to appear_before(ride2.name)
+  end
+
+end
