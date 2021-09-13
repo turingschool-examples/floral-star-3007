92f392124a870e0be5edb2d824cc81b2c3948036
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-13 10:48:50 -0600

    Add update route and action for mechanics

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -7,4 +7,11 @@ class MechanicsController < ApplicationController
     @mechanic = Mechanic.find(params[:mech_id])
     @ordered_rides = @mechanic.rides.order_by_thrill
   end
+
+  def update
+    mechanic = Mechanic.find(params[:mech_id])
+    mechanic.rides << Ride.find(params[:ride_id])
+
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
