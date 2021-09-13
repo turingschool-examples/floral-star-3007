1dbd6cdfdea2f9c97f3c8b6bf3cdeaf344ae5222
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-13 08:30:45 -0700

    Add relationships to spec

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Mechanic do
   describe 'relationship' do
-    it {should have_many :rides}
+    it {should have_many :ride_mechanics}
+    it {should have_many(:rides).through(:ride_mechanics)}
   end
 end
