c759edce28998fea239999699ca835f17f90b6a3
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-13 10:44:27 -0600

    Add tests for mechanic ahow page add ride form

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'the mechanics show page' do
     @ride_1 = @park.rides.create!(name: "Kiddie Coaster", thrill_rating: 3, open: true)
     @ride_2 = @park.rides.create!(name: "Rollercoaster", thrill_rating: 7, open: true)
     @ride_3 = @park.rides.create!(name: "Swings", thrill_rating: 5, open: false)
+    @ride_4 = @park.rides.create!(name: "Tower Drop", thrill_rating: 9, open: true)
 
     @mech_ride_1 = MechanicRide.create!(mechanic_id: @mech_1.id, ride_id: @ride_1.id)
     @mech_ride_2 = MechanicRide.create!(mechanic_id: @mech_1.id, ride_id: @ride_2.id)
@@ -35,6 +36,34 @@ RSpec.describe 'the mechanics show page' do
       expect(page).to have_content('Rollercoaster')
       expect(page).to_not have_content('Swings')
 
+      expect('Rollercoaster').to appear_before('Kiddie Coaster')
+    end
+  end
+
+  it "can add a ride to a mechanics list" do
+    visit "/mechanics/#{@mech_1.id}"
+    within('#rides') do
+      expect(page).to have_content('Kiddie Coaster')
+      expect(page).to have_content('Rollercoaster')
+      expect(page).to_not have_content('Tower Drop')
+    end
+
+    within('#add_ride') do
+      expect(page).to have_content("Add a ride to workload:")
+
+      expect(find('form')).to have_content('Ride Id')
+      fill_in 'Ride Id', with: @ride_4.id
+      click_button 'Submit'
+
+      expect(current_path).to eq("/mechanics/#{@mech_1.id}")
+    end
+
+    within('#rides') do
+      expect(page).to have_content('Tower Drop')
+      expect(page).to have_content('Kiddie Coaster')
+      expect(page).to have_content('Rollercoaster')
+
+      expect('Tower Drop').to appear_before('Rollercoaster')
       expect('Rollercoaster').to appear_before('Kiddie Coaster')
     end
   end
