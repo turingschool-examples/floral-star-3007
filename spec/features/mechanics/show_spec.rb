3a41ee872e29837ec44572b1988061a123a7a634
Erin Quinn <equinn125@gmail.com>
2021-09-13 10:00:55 -0600

    set up show page and show all rides mech has

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Mechanic index page' do
+RSpec.describe 'Mechanic show page' do
   before :each do
     @park_1 = AmusementPark.create!(name: "Six Flags", price_of_admission: 5)
     @ride_1 = @park_1.rides.create!(name: "Twister", thrill_rating: 4, open: true)
@@ -8,20 +8,17 @@ RSpec.describe 'Mechanic index page' do
     @ride_3 = @park_1.rides.create!(name: 'Bumper cars', thrill_rating:2, open:false)
     @kara = Mechanic.create!(name: 'Kara Smith', years_experience:3)
     @sam = Mechanic.create!(name: 'Sam Adams', years_experience:2)
+    @kara.rides << [@ride_1, @ride_2]
+    @sam.rides << [@ride_1, @ride_3]
   end
 
-  it ' displays the mechanics attributes' do
-    visit "/mechanics"
+  it 'shows mechanic attributes and all rides they are working on' do
+    visit "/mechanics/#{@kara.id}"
 
-    expect(page).to have_content("All Mechanics")
     expect(page).to have_content(@kara.name)
     expect(page).to have_content(@kara.years_experience)
-    expect(page).to have_content(@sam.name)
-    expect(page).to have_content(@sam.years_experience)
-  end
-
-  it 'displays the average years of experience accross all mechanics' do
-    visit "/mechanics"
-    expect(page).to have_content("Average years of experience:#{Mechanic.average_years}")
+    expect(page).to have_content(@ride_1.name)
+    expect(page).to have_content(@ride_2.name)
+    expect(page).to_not have_content(@ride_3.name)
   end
 end
