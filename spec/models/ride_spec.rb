91ba986d9e209bcfc8b23d1851d5d00b04cd0538
Christina Delpone <cdelpone@gmail.com>
2021-09-13 11:59:03 -0600

    test class methods

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Ride do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:mechanics).through(:mechanic_rides) }
   end
+
+  describe 'class methods' do
+    it 'returns only open rides' do
+      @fun_park = AmusementPark.create!(name: "Fun Park", price_of_admission: 10)
+      @best_ride = @fun_park.rides.create!(name: "Best Ride", thrill_rating: 2, open: true)
+      @scariest_ride = @fun_park.rides.create!(name: "Scariest Ride", thrill_rating: 10, open: true)
+      @closed_ride = @fun_park.rides.create!(name: "Closed Ride", thrill_rating: 6, open: false)
+
+      expect(Ride.available).to eq([@best_ride, @scariest_ride])
+    end
+
+    it 'returns rides in order of thrill rating' do
+      @fun_park = AmusementPark.create!(name: "Fun Park", price_of_admission: 10)
+      @best_ride = @fun_park.rides.create!(name: "Best Ride", thrill_rating: 2, open: true)
+      @scariest_ride = @fun_park.rides.create!(name: "Scariest Ride", thrill_rating: 10, open: true)
+      @closed_ride = @fun_park.rides.create!(name: "Closed Ride", thrill_rating: 6, open: false)
+    end
+  end
 end
