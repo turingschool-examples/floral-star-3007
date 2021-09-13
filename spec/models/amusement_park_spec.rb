f238494e319631e34a12607674241c84480558b4
Tanner Dale <tanner@tannerdale.com>
2021-09-13 11:37:51 -0500

    Fix average thrill rating expectation, add ordered rides test

--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -12,7 +12,11 @@ RSpec.describe AmusementPark do
     let!(:ride3) { park.rides.create!(name: 'Bungie Jump', thrill_rating: 10, open: true) }
 
     it 'has an average thrill rating' do
-      expect(park.average_thrill_rating).to eq(18)
+      expect(park.average_thrill_rating).to eq(6)
+    end
+
+    it 'has rides in alphabetical order' do
+      expect(park.ordered_rides).to eq([ride1, ride3, ride2])
     end
   end
 end
