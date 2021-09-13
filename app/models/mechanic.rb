68e03dedf0ee00313b93c16e160dfc01b8a90dd6
Tanner Dale <tanner@tannerdale.com>
2021-09-13 10:27:23 -0500

    Add test, scope for average years experience

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
+
+  scope :average_experience, -> {
+    average(:years_of_experience)
+  }
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
   it { should have_many(:rides).through(:mechanic_rides) }
+
+  describe 'class methods' do
+    let!(:mech1) { Mechanic.create!(name: 'Steve', years_of_experience: 10) }
+    let!(:mech2) { Mechanic.create!(name: 'Jen', years_of_experience: 8) }
+
+    it 'has the average years of experience' do
+      expect(Mechanic.average_experience).to eq(9)
+    end
+  end
 end
