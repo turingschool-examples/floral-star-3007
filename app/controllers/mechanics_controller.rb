c375a2afdf2e94b3a45b9f48d6437f0c5edeab5d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-13 09:51:01 -0700

    Add create action to controller

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -7,4 +7,12 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
   end
+
+  def create
+    mechanic = Mechanic.find(params[:id])
+    ride = Ride.find(params[:ride_id])
+    RideMechanic.create!(ride: ride, mechanic: mechanic)
+
+    redirect_to "/mechanics/#{params[:id]}"
+  end
 end
