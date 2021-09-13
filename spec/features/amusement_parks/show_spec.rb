a509b7e33b9a8f4ff629b1b9e0f220ef38b9f1ef
Tanner Dale <tanner@tannerdale.com>
2021-09-13 11:34:56 -0500

    Add amusement park ordered rides test, show view test

---
+++ spec/features/amusement_parks/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'amusement park show' do
+  let!(:park)  { AmusementPark.create(name: 'Six Flags', price_of_admission: 20) }
+  let!(:ride1) { park.rides.create!(name: '2F2F', thrill_rating: 6, open: true, amusement_park_id: park.id) }
+  let!(:ride2) { park.rides.create!(name: 'Splash Town', thrill_rating: 2, open: false) }
+  let!(:ride3) { park.rides.create!(name: 'Bungie Jump', thrill_rating: 10, open: true) }
+
+  before :each do
+    visit amusement_park_path(park)
+  end
+
+  it 'has the park name, admission price, average thrill rating' do
+    expect(page).to have_content(park.name)
+    expect(page).to have_content("$20.00")
+    expect(page).to have_content(park.average_thrill_rating)
+  end
+
+  it 'has its rides in alphabetical order' do
+    expect(ride1.name).to appear_before(ride3.name, only_text: true)
+    expect(ride3.name).to appear_before(ride2.name, only_text: true)
+  end
+end
+
--- spec/models/amusement_park_spec.rb
+++ spec/models/amusement_park_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe AmusementPark do
   describe 'relationships' do
     it { should have_many(:rides) }
   end
+
+  describe 'instance methods' do
+    let!(:park)  { AmusementPark.create(name: 'Six Flags', price_of_admission: 20) }
+    let!(:ride1) { park.rides.create!(name: '2F2F', thrill_rating: 6, open: true, amusement_park_id: park.id) }
+    let!(:ride2) { park.rides.create!(name: 'Splash Town', thrill_rating: 2, open: false) }
+    let!(:ride3) { park.rides.create!(name: 'Bungie Jump', thrill_rating: 10, open: true) }
+
+    it 'has an average thrill rating' do
+      expect(park.average_thrill_rating).to eq(18)
+    end
+  end
 end
