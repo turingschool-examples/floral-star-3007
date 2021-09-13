94384bf70122e507d876a7c30c342cb42627586f
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-13 09:50:47 -0700

    Handroll routes, resources result in no POST route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :mechanics, only: [:index, :show]
+  # resources :mechanics, only: [:index, :show, :create]
+  
+  get "/mechanics", to: "mechanics#index"
+  get "/mechanics/:id", to: "mechanics#show"
+  post "/mechanics/:id", to: "mechanics#create"
 end
