88d4e6f4e70abd6c9e62aacfa0fd1919df46a1a3
Andrew Massey <acmassey@ncsu.edu>
2021-09-13 11:58:39 -0400

    Test and write #average_experience

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'mechanics index page' do
 
   before :each do
-    mechanic_1 = Mechanic.create!(name: "Manny", experience: 10)
-    mechanic_2 = Mechanic.create!(name: "Moe", experience: 15)
-    mechanic_3 = Mechanic.create!(name: "Jack", experience: 20)
+    @mechanic_1 = Mechanic.create!(name: "Manny", experience: 10)
+    @mechanic_2 = Mechanic.create!(name: "Moe", experience: 15)
+    @mechanic_3 = Mechanic.create!(name: "Jack", experience: 20)
   end
 
   it "lists all mechanic names on the index page" do
