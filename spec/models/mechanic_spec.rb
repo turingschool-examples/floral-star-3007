67d5ba81ac548eb074720f0efb38a77465d1f25d
Ida Olson <idaolson@gmail.com>
2021-09-13 09:46:20 -0600

    Add test to mechanic model spec

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Mechanic do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
   end
+
+  it "averages years of experience" do
+    mech1 = Mechanic.create!(name: "Ida Olson", years_experience: 14)
+    mech2 = Mechanic.create!(name: "Sarah Carter", years_experience: 10)
+
+    expect(Mechanic.average_experience).to eq(12)
+  end
 end
