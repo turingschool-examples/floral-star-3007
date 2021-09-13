8bef7d516758acf7219f9f3a535adacd15fe5810
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-13 09:53:37 -0600

    Add test for avg_xp class method

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Mechanic, type: :model do
+  before(:each) do
+    @kara = Mechanic.create!(name: "Kara Johnson", yrs_xp: 12)
+    @willie = Mechanic.create!(name: "Willie Walsh", yrs_xp: 32)
+  end
   describe 'relationships' do
     it {should have_many(:mechanic_rides)}
     it {should have_many(:rides).through(:mechanic_rides)}
+  end
+
+  describe 'class methods' do
+    it 'has average experience for all' do
+      expect(Mechanic.avg_xp).to eq(22)
+    end
   end
 end
